6e110565a5e7e5e0d75e43148deed661efbf6fd9
xuhaiyan <410780496@qq.com>
2017-05-14 22:06:57 +0800

    'init'

---
+++ src/components/hello.jsx
@@ -0,0 +1,8 @@
+import {React}, Component from 'React'
+
+class Welcome extends Component {
+  render() {
+    return <h1>Hello, 123123</h1>;
+  }
+}
+
---
+++ src/main.js
@@ -0,0 +1,45 @@
+/*
+created by luyixin on 2017.05.13
+*/
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Clock extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount () {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Hello, world!333</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Clock/>,
+  document.getElementById('root')
+)
+
